62189051ea1ac64a17f259110d89885708bb7684
MiguelLm84 <72008078+MiguelLm84@users.noreply.github.com>
2022-09-09 01:03:44 +0200

    mejoras

--- app/src/main/java/com/miguel_lm/shiftdispenser/MainActivity.java
+++ app/src/main/java/com/miguel_lm/shiftdispenser/MainActivity.java
@@ -2,10 +2,13 @@ package com.miguel_lm.shiftdispenser;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.net.http.SslError;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
+import android.webkit.SslErrorHandler;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -19,6 +22,9 @@ public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
     //static final String URL = "http://192.180.2.31:8082/gmedia4_5_3/expendedor.php";
     static final String URL = "http://rubenhermida.com";
+    //static final String SEARCH_PATH = "/search?q=rubenhermida.com";
+    //static final String SEARCH_PATH = "/search?q=192.180.2.31:8082/gmedia4_5_3/expendedor.php";
+    //static final String URL = "https://google.es" + SEARCH_PATH;
 
     //private static final String ACTION_USB_PERMISSION = "com.android.example.USB_PERMISSION";
     //UsbConnection usbConnection = UsbPrintersConnections.selectFirstConnected(this);
@@ -44,19 +50,51 @@ public class MainActivity extends AppCompatActivity {
         binding.webViewShiftDispenser.getSettings().setJavaScriptEnabled(true);
         binding.webViewShiftDispenser.getSettings().setBuiltInZoomControls(true);
         binding.webViewShiftDispenser.getSettings().setDisplayZoomControls(false);
+        binding.webViewShiftDispenser.getSettings().setDomStorageEnabled(true);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            binding.webViewShiftDispenser.getSettings().setSafeBrowsingEnabled(true);
+            binding.webViewShiftDispenser.getSettings().setSafeBrowsingEnabled(false);
         }
         binding.webViewShiftDispenser.setWebViewClient(new WebViewClient() {
 
             @Override
+            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+                handler.proceed(); // Ignore SSL certificate errors
+            }
+
+            /*@Override
             public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
 
-                Intent intent = new Intent(Intent.ACTION_VIEW, request.getUrl());
+                /*Intent intent = new Intent(Intent.ACTION_VIEW, request.getUrl());
                 startActivity(intent);
                 overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
-                return true;
-            }
+                return true;*/
+                //return false;
+            //}
+
+            /*@Override
+            public void onReceivedSslError(WebView view, final SslErrorHandler handler, SslError error) {
+                String message = "SSL Certificate error.";
+
+                switch (error.getPrimaryError()) {
+                    case SslError.SSL_UNTRUSTED:
+                        message = "The certificate authority is not trusted.";
+                        break;
+                    case SslError.SSL_EXPIRED:
+                        message = "The certificate has expired.";
+                        break;
+                    case SslError.SSL_IDMISMATCH:
+                        message = "The certificate Hostname mismatch.";
+                        break;
+                    case SslError.SSL_NOTYETVALID:
+                        message = "The certificate is not yet valid.";
+                        break;
+                }
+                message += "\"SSL Certificate Error\" Do you want to continue anyway?.. YES";
+
+                handler.proceed();
+
+                Log.e("TAG", "onReceivedSslError : " + message);
+            }*/
         });
         binding.webViewShiftDispenser.loadUrl(URL);
     }
